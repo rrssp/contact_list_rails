9a26dee4e6b0b37d6f13fc847db3b27f1caaed78
rrssp <romaosp17@gmail.com>
2022-09-05 17:34:37 -0300

    Paginas estaticas

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,10 @@ class StaticPagesController < ApplicationController
     @nome = "Bruno"
   end
 
+  def sobre
+  end
+
+  def contato
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root to: "static_pages#index"
-
-
+  root            to: 'static_pages#index'
+  get 'sobre',    to: 'static_pages#sobre'
+  get 'contato',  to: 'static_pages#contato'
 
 end
